051485365ec4d607804d02a4ffb99be2bb3d9088
uthran <uthiranuthiran22@gmail.com>
2026-04-10 13:22:16 +0530

    changed

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './login.css'
-import { mainIcon } from '../../assets/Main-Icon.png'
+import  mainIcon  from '../../assets/Main-Icon.png'
 import { Hand } from 'lucide-react';
 import toast from 'react-hot-toast';
 import { useAuth } from '../../Context/AuthContext';
